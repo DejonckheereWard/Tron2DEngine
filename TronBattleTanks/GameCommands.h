5c493baf4fb18f94bebb4b1357e879c6d43b74a0
Dejonckheere Ward <ward.dejonckheere@student.howest.be>
2023-05-14 20:06:13 +0200

    Cleaned up old commands and test code

--- TronBattleTanks/GameCommands.h
+++ TronBattleTanks/GameCommands.h
@@ -6,40 +6,6 @@
 
 using Engine::Command;
 
-
-// Testing
-class OnReleaseCommand final: public Command
-{
-public:
-	// Inherited via Command
-	virtual void Execute(const glm::vec2&) override
-	{
-		std::cout << "OnReleaseCommand\n";
-	};
-};
-
-class OnPressCommand final: public Command
-{
-public:
-	// Inherited via Command
-	virtual void Execute(const glm::vec2&) override
-	{
-		std::cout << "OnPressCommand\n";
-		Engine::ServiceLocator::GetAudioService().PlayEffect("Sounds/Background.mp3", 1.0f);
-	};
-};
-
-class PressedCommand final: public Command
-{
-public:
-	// Inherited via Command
-	virtual void Execute(const glm::vec2&) override
-	{
-		std::cout << "PressedCommand\n";
-	};
-};
-
-
 // Game commands
 class MoveCommand final: public Command
 {
@@ -62,50 +28,6 @@ public:
 private:
 	glm::vec2 m_MoveDirection;
 	float m_MovementSpeed;
-};
-
-
-class DamagePlayer final: public Command
-{
-public:
-	DamagePlayer(Engine::GameObject* actor, int dmgAmount):
-		Command(actor),
-		m_DamageAmount{ dmgAmount }
-	{
-		m_pHealthComponent = GetOwner()->GetComponent<HealthComponent>();
-	};
-
-	// Inherited via Command
-	virtual void Execute(const glm::vec2&) override
-	{
-		m_pHealthComponent->TakeDamage(m_DamageAmount);
-	};
-
-private:
-	HealthComponent* m_pHealthComponent;
-	int m_DamageAmount;
-};
-
-class AddScore final: public Command
-{
-public:
-	AddScore(Engine::GameObject* actor, int scoreAmount):
-		Command(actor),
-		m_ScoreAmount{ scoreAmount }
-	{
-		m_pScoreComponent = GetOwner()->GetComponent<ScoreComponent>();
-	};
-
-	// Inherited via Command
-	virtual void Execute(const glm::vec2&) override
-	{
-		m_pScoreComponent->AddScore(m_ScoreAmount);
-	};
-
-private:
-	ScoreComponent* m_pScoreComponent;
-	int m_ScoreAmount;
-
 };
 
 class AimTurretCommand final: public Command
